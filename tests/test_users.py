430eb8bc23ed7d9d5f1a946bb5e0f26bf5bbdf99
Vítor <vitorhugo8899o@gmail.com>
2026-05-02 11:00:50 -0300

    test: email já existe/ rota de criação de conta

--- tests/test_users.py
+++ tests/test_users.py
@@ -21,3 +21,21 @@ async def test_create_user(client):
     assert response['email'] == 'uber@gmail.com'
     assert response['role'] == 'cliente'
     assert 'created_at' in response
+
+
+@pytest.mark.asyncio
+async def test_email_alredy_in_use(client, user_client):
+    payload = {
+        'fullname': 'Nome Completo',
+        'email': 'user@example.com',
+        'password': 'Senha12@#',
+    }
+
+    req = await client.post('/api/v1/users', json=payload)
+
+    status = 409
+
+    assert req.status_code == status
+    assert req.json()['detail'] == 'Esse endereço de Email já está em uso!'
+
+
