65fcf8e6f1314cfbbb1023009eea7071cde16cb2
Vítor <vitorhugo8899o@gmail.com>
2026-05-09 16:09:22 -0300

    fix: arrumando função que deu erro na branch

--- app/api/v1/endpoints/psychologist.py
+++ app/api/v1/endpoints/psychologist.py
@@ -6,7 +6,10 @@ from fastapi import APIRouter
 from app.api.v1.dependencies import CurrentUser, DBSession, rediscon
 from app.api.v1.services import appoint_service, psych_service
 from app.schemas.appointment_schema import AppointmentUserResponse
-from app.schemas.psychologist_schema import PsychologistAvaliabiliteCreate
+from app.schemas.psychologist_schema import (
+    AvailabilityCacheSchema,
+    PsychologistAvaliabiliteCreate,
+)
 
 psych_router = APIRouter()
 
@@ -31,3 +34,12 @@ async def create_avaliabilite(
 )
 async def get_appiontment(db: DBSession, user: CurrentUser):
     return await appoint_service.get_psych_appointment(db, user)
+
+
+@psych_router.get(
+    '/psych/me/availability',
+    status_code=HTTPStatus.OK,
+    response_model=List[AvailabilityCacheSchema]
+)
+async def get_schedule(db: DBSession, r: rediscon, user: CurrentUser):
+    return await psych_service.get_avaliabilites(db, r, user)
--- app/api/v1/repositories/psych_repo.py
+++ app/api/v1/repositories/psych_repo.py
@@ -3,9 +3,10 @@ from zoneinfo import ZoneInfo
 
 from sqlalchemy import and_, exists, select
 
-from app.api.v1.dependencies import DBSession
+from app.api.v1.dependencies import DBSession, rediscon
 from app.models.avaliabilites_models import Avaliabilite
 from app.models.psychologist_models import Psychologist
+from app.schemas.psychologist_schema import availability_list_adapter
 
 
 async def get_psych(db: DBSession, id_psych: int) -> Psychologist | None:
@@ -65,3 +66,36 @@ async def avaliabilite_exists(
     result = await db.execute(stmt)
 
     return result.scalar()
+
+
+async def cache_avaliabilites(db: DBSession, r: rediscon, psych_id: int):
+    cache_key = f"psychologist:{psych_id}:full_schedule"
+
+    avaliabilite_cache = await r.get(cache_key)
+
+    if avaliabilite_cache:
+        return availability_list_adapter.validate_json(avaliabilite_cache)
+
+    stmt = select(Avaliabilite).where(
+        Avaliabilite.id_psychologist == psych_id
+    ).order_by(Avaliabilite.day_of_the_week)
+
+    result = await db.execute(stmt)
+    db_availabilities = result.scalars().all()
+
+    if not db_availabilities:
+        return []
+
+    pydantic_availabilities = availability_list_adapter.validate_python(
+        db_availabilities
+    )
+
+    schedule_json = availability_list_adapter.dump_json(pydantic_availabilities)
+
+    await r.set(
+        cache_key,
+        schedule_json,
+        40400
+    )
+
+    return pydantic_availabilities
